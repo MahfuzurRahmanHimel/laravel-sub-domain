e11b13e5b84be00231437ff71555d9a5100ea8ed
iktakhaiurl <iktakhairul@gmail.com>
2021-06-14 15:34:19 +0600

    User Index Details Design

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -5,37 +8,41 @@
         body {
             font-family: 'Nunito', sans-serif;
         }
-        .home {
-            margin: 50px;
-        }
-        .dashboard-item{
-            margin-left: 30px;
-        }
         .category-block{
             font-weight: normal;
+            font-size: 16px;
+        }
+        .bold{
+            font-weight: bold;
         }
     </style>
 </head>
 <body class="antialiased">
-<div class="container home">
-    <h3>Domains User</h3>
-    <div class="col-md-3 category-responsive dashboard-item">
-        <h4>This is {{$users->domain}} users panel</h4>
-        <h4 class="category-block">Role: {{$users->role}}</h4>
-    </div>
-</div>
-<div class="container home">
-    <h3>User Details</h3>
-    <div class="col-md-3 category-responsive dashboard-item">
-        <h4 class="category-block">Name: {{$users->name}}</h4>
-        <h4 class="category-block">Email: {{$users->email}}</h4>
-        <h4 class="category-block">Role: {{$users->role}}</h4>
-        <h4 class="category-block">Contact No: {{$users->contact_no}}</h4>
-        <h4 class="category-block">Type: {{$users->type}}</h4>
-        <h4 class="category-block">Weight: {{$users->weight}}</h4>
-        <h4 class="category-block">Access: {{$users->access}}</h4>
-        <h4 class="category-block">Status: {{$users->status}}</h4>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">This is {{$users->domain}}{{ __(' user\'s panel.') }}</div>
+                <div class="card-body">
+                    <div class="container home">
+                        <p class="category-block bold">User Details:</p>
+                        <div class="category-responsive dashboard-item">
+                            <p class="category-block">Name: {{$users->name}}</p>
+                            <p class="category-block">Email: {{$users->email}}</p>
+                            <p class="category-block">Role: {{$users->role}}</p>
+                            <p class="category-block">Contact No: {{$users->contact_no}}</p>
+                            <p class="category-block">Type: {{$users->type}}</p>
+                            <p class="category-block">Weight: {{$users->weight}}</p>
+                            <p class="category-block">Access: {{$users->access}}</p>
+                            <p class="category-block">Status: {{$users->status}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </body>
 </html>
+@endsection
